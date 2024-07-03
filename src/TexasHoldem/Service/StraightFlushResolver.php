5e37946016bd99bc97c04491e79f30491974d741
Artem Tyutnev <artem.tiutnev@gmail.com>
2024-07-03 17:53:53 +0300

    Добавлен сервис для проверки комбинации - Стрит Флэш

---
+++ src/TexasHoldem/Service/StraightFlushResolver.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace ForestYeti\TritonEngine\TexasHoldem\Service;
+
+use ForestYeti\TritonEngine\Common\Poker\DTO\ResolverResult;
+use ForestYeti\TritonEngine\GameCard\Entity\GameCardEntity;
+use ForestYeti\TritonEngine\Common\Poker\Service\ResolverInterface;
+use ForestYeti\TritonEngine\GameCard\Repository\GameCardDeck;
+
+class StraightFlushResolver implements ResolverInterface
+{
+    private const NAME     = 'StraightFlush';
+    private const PRIORITY = 10;
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getPriority(): int
+    {
+        return self::PRIORITY;
+    }
+
+    /**
+     * @param GameCardEntity[] $pockerCards
+     * @param GameCardEntity[] $boardCards
+     */
+    public function execute(array $pocketCards, array $boardCards): ResolverResult
+    {
+        $gameCards      = array_merge($pocketCards, $boardCards);
+        $groupedBySuits = [];
+
+        foreach ($gameCards as $gameCard) {
+            if (!isset($groupedBySuits[$gameCard->getSuit()])) {
+                $groupedBySuits[$gameCard->getSuit()] = [];
+            }
+
+            $groupedBySuits[$gameCard->getSuit()][] = $gameCard;
+        }
+
+        $targetSuit = null; 
+        foreach ($groupedBySuits as $suit => $cardsBySuit) {
+            if (count($cardsBySuit) < 5) {
+                continue;
+            }
+
+            $gameCards = (new GameCardDeck($cardsBySuit))->sortAsc();
+            $counter = 1;
+            for ($i = 0; $i < count($gameCards) - 1; $i++) {
+                if ($this->hasStraightFlushDro($gameCards[$i], $gameCards[$i + 1])) {
+                    $counter++;
+                }
+            }
+
+            if ($counter >= 5) {
+                $targetSuit = $suit;
+                break;
+            }
+        }
+
+        if ($targetSuit === null) {
+            return new ResolverResult(self::NAME, self::PRIORITY, false);
+        }
+
+        $kicker = null;
+        if ($pocketCards[1]->getSuit() === $targetSuit) {
+            $kicker = $pocketCards[1];
+        } else if ($pocketCards[0]->getSuit() === $targetSuit) {
+            $kicker = $pocketCards[0];
+        }
+
+        return new ResolverResult(self::NAME, self::PRIORITY, true, $kicker);
+    }
+
+    private function hasStraightFlushDro(GameCardEntity $first, GameCardEntity $next): bool
+    {
+        return 
+            $first->getRank() + 1 === $next->getRank() && 
+            $first->getSuit() === $next->getSuit();
+    }
+}
---
+++ tests/TexasHoldem/StraightFlushResolverTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ForestYeti\Tests\TexasHoldem;
+
+use ForestYeti\TritonEngine\GameCard\Enum\RankEnum;
+use ForestYeti\TritonEngine\GameCard\Enum\SuitEnum;
+use ForestYeti\TritonEngine\GameCard\Service\GameCardBuilder;
+use ForestYeti\TritonEngine\TexasHoldem\Service\StraightFlushResolver;
+use PHPUnit\Framework\TestCase;
+
+class StraightFlushResolverTest extends TestCase
+{
+    private StraightFlushResolver $straightFlushResolver;
+    private GameCardBuilder       $gameCardBuilder;
+
+    public function setUp(): void
+    {
+        $this->straightFlushResolver = new StraightFlushResolver();
+        $this->gameCardBuilder       = new GameCardBuilder();
+    }
+
+    public function testStraightFlushExistWithKicker()
+    {
+        $pocketCards = [
+            $this->gameCardBuilder->build(RankEnum::Five, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Six, SuitEnum::Diamods),
+        ];
+
+        $boardCards = [
+            $this->gameCardBuilder->build(RankEnum::Nine, SuitEnum::Hearts),
+            $this->gameCardBuilder->build(RankEnum::Seven, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Ace, SuitEnum::Spades),
+            $this->gameCardBuilder->build(RankEnum::Nine, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Eight, SuitEnum::Diamods),
+        ];
+
+        $resolverResult = $this->straightFlushResolver->execute($pocketCards, $boardCards);
+
+        $this->assertTrue($resolverResult->isCombinationExist());
+        $this->assertEquals(RankEnum::Six->value, $resolverResult->getKicker()->getRank());
+        $this->assertEquals(SuitEnum::Diamods->value, $resolverResult->getKicker()->getSuit());
+    }
+
+    public function testStraightFlushExistWithoutKicker()
+    {
+        $pocketCards = [
+            $this->gameCardBuilder->build(RankEnum::King, SuitEnum::Spades),
+            $this->gameCardBuilder->build(RankEnum::Ace, SuitEnum::Hearts),
+        ];
+
+        $boardCards = [
+            $this->gameCardBuilder->build(RankEnum::Six, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Seven, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Five, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Nine, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Eight, SuitEnum::Diamods),
+        ];
+
+        $resolverResult = $this->straightFlushResolver->execute($pocketCards, $boardCards);
+
+        $this->assertTrue($resolverResult->isCombinationExist());
+        $this->assertNull($resolverResult->getKicker());
+    }
+
+    public function testStraightFlushNotExist()
+    {
+        $pocketCards = [
+            $this->gameCardBuilder->build(RankEnum::Five, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Six, SuitEnum::Diamods),
+        ];
+
+        $boardCards = [
+            $this->gameCardBuilder->build(RankEnum::Nine, SuitEnum::Hearts),
+            $this->gameCardBuilder->build(RankEnum::Seven, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Ace, SuitEnum::Spades),
+            $this->gameCardBuilder->build(RankEnum::Nine, SuitEnum::Diamods),
+            $this->gameCardBuilder->build(RankEnum::Eight, SuitEnum::Hearts),
+        ];
+
+        $resolverResult = $this->straightFlushResolver->execute($pocketCards, $boardCards);
+
+        $this->assertFalse($resolverResult->isCombinationExist());
+        $this->assertNull($resolverResult->getKicker());
+    }
+}
